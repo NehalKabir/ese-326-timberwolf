2428c9f8fe6a410eb1ba54dcb94077cd94913d32
tomplo18 <112023770+tomplo18@users.noreply.github.com>
2023-05-02 20:02:43 -0400

    Update Net.h

--- Net.h
+++ Net.h
@@ -1,45 +1,20 @@
 #pragma once
 #include <string>
 #include <vector>
+#include "Cell.h"
 
 using namespace std;
 
 class Net
 {
 private:
-	string firstNode;
-	vector<string> nodes;
+	Cell firstNode;
+	vector<Cell> nodes;
 public:
-	Net(string f, vector<string> n);
-	string getFirstNode();
-	void setFirstNode(string f);
-	vector<string> getNodes();
-	void setNodes(vector<string> n);
+	Net(Cell f, vector<Cell> n);
+	Cell getFirstNode();
+	void setFirstNode(Cell f);
+	vector<Cell> getNodes();
+	void setNodes(vector<Cell> n);
 
 };
-
-Net::Net(string f, vector<string> n)
-{
-	firstNode = f;
-	nodes = n;
-}
-
-string Net::getFirstNode()
-{
-	return firstNode;
-}
-
-void Net::setFirstNode(string f)
-{
-	firstNode = f;
-}
-
-vector<string> Net::getNodes()
-{
-	return nodes;
-}
-
-void Net::setNodes(vector<string> n)
-{
-	nodes = n;
-}
