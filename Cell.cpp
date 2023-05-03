bcf46d04d04f0a3b277fc3ab5ad430f0d5d2aec1
tomplo18 <112023770+tomplo18@users.noreply.github.com>
2023-05-02 20:01:56 -0400

    Update Cell.cpp

--- Cell.cpp
+++ Cell.cpp
@@ -2,10 +2,20 @@
 
 using namespace std;
 
-Cell::Cell(string i, int w)
+Cell::Cell()
+{
+	id = "error-default";
+	weight = 0;
+	r = 0;
+	c = 0;
+}
+
+Cell::Cell(string i, int w, int x, int y)
 {
 	id = i;
 	weight = w;
+	r = x;
+	c = y;
 }
 
 string Cell::getId()
@@ -26,4 +36,24 @@ int Cell::getWeight()
 void Cell::setWeight(int w)
 {
 	weight = w;
+}
+
+int Cell::getR()
+{
+	return r;
+}
+
+void Cell::setR(int x)
+{
+	r = x;
+}
+
+int Cell::getC()
+{
+	return c;
+}
+
+void Cell::setC(int y)
+{
+	c = y;
 }
