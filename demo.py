32b1dfffb0868dc1302c4e3f885936bd72d18bff
NehalKabir <92537145+NehalKabir@users.noreply.github.com>
2023-05-07 17:38:28 -0400

    Update demo.py

--- demo.py
+++ demo.py
@@ -6,11 +6,21 @@
 
 import matplotlib
 import matplotlib.pyplot as plt
+import numpy as np
 f = open("demo.txt", "r")
+net = open("ibm01.net", "r")
+area = open("ibm01.are","r")
+areaval = []
+netval = []
+
 values = []
+values2 = []
 temp =[]
-xval =[]
-yval = []
+temp2 = []
+xval1 =[]
+yval1 = []
+xval2 =[]
+yval2 = []
 weightval = []
 check = []
 data = f.readlines()
@@ -20,7 +30,21 @@ for line in data:
         
     else:
         values.append(line)
+        
+data3 = area.readlines()
+for line in data3:
+    if line[-1] == '\n':
+        values2.append(line[:-1])
+        
+    else:
+        values2.append(line)
+        
 
+
+for string in values2:
+    substrings = string.split(" ")
+    temp2.extend(substrings)
+    
 for string in values:
     substrings = string.split(" ")
     temp.extend(substrings)
@@ -31,37 +55,40 @@ print("this is xval")
 for line in data:
     
     substrings = line.split()
-    xval.append(int(substrings[0]))
-    yval.append(int(substrings[1]))
+    xval1.append(int(substrings[1]))
+    yval1.append(int(substrings[0]))
     weightval.append(int(substrings[2]))
+    #xval2.append(int(substrings[4]))
+    #yval2.append(int(substrings[5]))
 
-    
+for line in data3:
+    substring = line.split()
+    areaval.append(int(substring[1]))
 
 
-print(xval)
-print(yval)
-print(weightval)
+#print(xval1)
+#print(yval1)
+#print(xval2)
+#print(yval2)
+#print(weightval)
+#print(areaval)
 
-
+#for plot get x1 y1 and x2 y2 in their own array then we can plot just gotaa gigure out how 
 
 #col and row are lit pos of the table
 fig = plt.figure()
 ax = fig.add_subplot(111)
+colors = np.random.rand(len(xval1), 3)
 
-for x in range(len(xval)):
-    
-    rect1 = matplotlib.patches.Rectangle((xval[x],yval[x]),weightval[x],1, linewidth = 1, edgecolor = 'b', facecolor ='none')
-    plt.plot([1,3], [2,4])
+
+for x in range(len(xval1)):
+    rectangle_color = colors[x]
+    rect1 = matplotlib.patches.Rectangle((xval1[x],yval1[x]),areaval[x],1, linewidth = 1, edgecolor = rectangle_color, facecolor ='none')
+    #plt.plot([xval1[x], yval1[x]], [xval2[x], yval2[x]])
     ax.add_patch(rect1)
-    
 
-#
-#x y 
-#rect works with (x,y) cord so x will be row y will increment to act as col third param is width so its area
-#4th param will remain as a constant of 1 since all height is the same 
-
-plt.xlim([0, 6])
-plt.ylim([0, 6])
+plt.xlim([0, 700000])
+plt.ylim([0, 1])
 
 #plt.show()
 
@@ -77,6 +104,10 @@ plt.ylim([0, 6])
 #display plot
 plt.show()
 #had to change setting so that it wouldnt only read C drive and would read the folder that the code was in 
+
+
+
+
 
 
 
