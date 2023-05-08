a91f7942ca16052449b1076345688e51920bdbe9
tomplo18 <112023770+tomplo18@users.noreply.github.com>
2023-05-07 22:21:31 -0400

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -5,6 +5,7 @@
 #include <vector>
 #include <fstream>
 #include <math.h>
+#include <cmath>
 #include <random>
 #include <algorithm>
 #include <map>
@@ -13,11 +14,15 @@ using namespace std;
 
 Cell findCell(string id, vector<vector<Cell>> placement);
 Cell findCell2(string id, map<string, Cell> &hash);
+void updateHash(vector<vector<Cell>> placement, map<string, Cell>& hash);
+vector<Net> updateNets(vector<vector<Cell>> placement, map<string, Cell>& hash);
 float Cost1(vector<vector<Cell>> placement, vector<Net> paths); //total nets
 float Cost2(vector<vector<Cell>> placement, vector<Net> paths); //critical net
 float Cost3(vector<vector<Cell>> placement); //row difference
 float Cost4(vector<vector<Cell>> placement, vector<Net> paths); //congestion
-float normalizeCost(float c1, float c2, float c3, float maxC1, float maxC2, float maxC3);
+float normalizeCost(float c1, float c2, float c3, float c4, float maxC1, float maxC2, float maxC3, float maxC4);
+float increaseProbability(float deltaC, float temp);
+float tempSchedule(float curTemperature);
 vector<vector<Cell>> Perturb(vector<vector<Cell>> table);
 
 int main()
@@ -35,14 +40,13 @@ int main()
 	int numrows = 6;
 	int rowWeight;
 	vector<Cell> templst;
-	vector<vector<Cell>> table;
+	vector<vector<Cell>> table, newTable;
 	map<string, Cell> hash;
 	ifstream weights;
 	ifstream nets;
 
 	string NetTemp;
 	string NetTemp2;
-	string NetTemp3;
 	string NetTemp4;
 	vector <Cell> nodetemp;
 	int index;
@@ -51,8 +55,9 @@ int main()
 
 	float curTemperature = 4000000;
 	float finalTemperature = .1;
-	float maxCost1, maxCost2, maxCost3;
+	float maxCost1, maxCost2, maxCost3, maxCost4;
 	float curCost, newCost;
+	float randNum;
 
 	srand(time(NULL));
 
@@ -85,7 +90,6 @@ int main()
 			templst.back().setC(colCount);
 			table[count].push_back(templst.back());
 			curWeight += templst.back().getWeight();
-			hash.insert(pair<string, Cell>(templst.back().getId(), templst.back()));
 			//cout << hash.begin() ->first << endl;
 			templst.pop_back();
 			totalCells++;
@@ -94,9 +98,119 @@ int main()
 		curWeight = 0;
 		count++;
 	}
+
 	//initial placement created
 	cout << "init placed" << endl;
 
+	updateHash(table, hash);
+
+	cout << "hash made" << endl;
+
+	templst2 = updateNets(table, hash);
+
+	cout << "nets set" << endl;
+
+	//for (int i = 0; i < templst2.size(); i++) {
+	//	// Printing the element at
+	//	// index 'i' of vector
+	//	cout << templst2[i].getFirstNode().getId() << endl;
+	//	for (Cell j : templst2[i].getNodes()) {
+	//		cout << j.getId() << endl;
+	//	}
+	//}
+
+	//cout << Cost1(table, templst2) << endl;
+	//cout << Cost2(table, templst2) << endl;
+	//cout << Cost3(table) << endl;
+	//cout << Cost4(table, templst2) << endl;
+
+	//make the max Costs so we can normalize and compare later
+	maxCost1 = Cost1(table, templst2);
+	maxCost2 = Cost2(table, templst2);
+	maxCost3 = Cost3(table);
+	maxCost4 = Cost4(table, templst2);
+	curCost = normalizeCost(maxCost1, maxCost2, maxCost3, maxCost4, maxCost1, maxCost2, maxCost3, maxCost4);
+	while (curTemperature > finalTemperature)
+	{
+		for (int i = 0; i < (totalCells/500); i++)
+		{
+			newTable = Perturb(table);
+			newCost = normalizeCost(Cost1(newTable, templst2), Cost2(newTable, templst2), Cost3(newTable), Cost4(newTable, templst2), 
+				maxCost1, maxCost2, maxCost3, maxCost4);
+			//cout << newCost <<endl;
+
+			randNum = (float)rand() / RAND_MAX;
+			//cout << randNum << endl;
+
+			if (newCost < curCost)
+			{
+				table = newTable;
+				updateHash(table, hash);
+				templst2 = updateNets(table, hash);
+				curCost = newCost;
+			}
+			else if (randNum > increaseProbability((newCost - curCost), curTemperature) && curTemperature > 100)
+			{
+				table = newTable;
+				updateHash(table, hash);
+				templst2 = updateNets(table, hash);
+				curCost = newCost;
+				cout << "Random jump up!" << endl;
+			}
+		}
+		curTemperature = tempSchedule(curTemperature);
+		cout << curCost << ", " << curTemperature << endl;
+	}
+}
+
+Cell findCell(string id, vector<vector<Cell>> placement)
+{
+	for (int j = 0; j < placement.size(); j++) // finds coords of starting Cell
+	{
+		for (int k = 0; k < placement[j].size(); k++)
+		{
+			if (placement[j][k].getId() == id)
+			{
+				return placement[j][k];
+			}
+		}
+	}
+	cout << "ERROR - findCell found nothing" << endl;
+	return Cell();
+}
+
+Cell findCell2(string id, map<string, Cell> &hash)
+{
+	if (hash.count(id) ) {
+//		cout << hash[id].getWeight() << endl;
+		return hash[id];
+	}
+	cout << "ERROR - findCell found nothing" << endl;
+	return Cell();
+}
+
+void updateHash(vector<vector<Cell>> placement, map<string, Cell>& hash)
+{
+	for (int i = 0; i < placement.size(); i++) {
+		for (int j = 0; j < placement[i].size(); j++) {
+			hash[placement[i][j].getId()] = placement[i][j];
+		}
+	}
+}
+
+vector<Net> updateNets(vector<vector<Cell>> placement, map<string, Cell>& hash)
+{
+	Cell c("test", 1001, 0, 0);
+	Net n(c, { c });
+	string NetTemp;
+	string NetTemp2;
+	string NetTemp4;
+	vector <Cell> nodetemp;
+	int index;
+	vector<Net> templst2;
+	int snt2;
+	ifstream nets;
+	
 	nets.open("ibm01.net", ios::in);
 	if (nets.fail()) {
 		cerr << "This file does not exist." << endl;
@@ -130,59 +244,9 @@ int main()
 	}
 	n.setNodes(nodetemp);
 	templst2.push_back(n);
-	cout << "nets recorded" << endl;
-	//for (int i = 0; i < templst2.size(); i++) {
-	//	// Printing the element at
-	//	// index 'i' of vector
-	//	cout << templst2[i].getFirstNode() << endl;
-	//	for (string j : templst2[i].getNodes()) {
-	//		cout << j << endl;
-	//	}
-	//}
 
-	//cout << Cost1(table, templst2) << endl;
-	//cout << Cost2(table, templst2) << endl;
-	//cout << Cost3(table) << endl;
-	cout << Cost4(table, templst2) << endl;
-
-	//make the max Costs so we can normalize and compare later
-	maxCost1 = Cost1(table, templst2);
-	maxCost2 = Cost2(table, templst2);
-	maxCost3 = Cost3(table);
-	curCost = normalizeCost(maxCost1, maxCost2, maxCost3, maxCost1, maxCost2, maxCost3);
-	//while (curTemperature > finalTemperature)
-	//{
-	//	for (int i = 0; i < (totalCells * 800); i++)
-	//	{
-
-	//	}
-	//}
-}
-
-Cell findCell(string id, vector<vector<Cell>> placement)
-{
-	for (int j = 0; j < placement.size(); j++) // finds coords of starting Cell
-	{
-		for (int k = 0; k < placement[j].size(); k++)
-		{
-			if (placement[j][k].getId() == id)
-			{
-				return placement[j][k];
-			}
-		}
-	}
-	cout << "ERROR - findCell found nothing" << endl;
-	return Cell();
-}
-
-Cell findCell2(string id, map<string, Cell> &hash)
-{
-	if (hash.count(id) ) {
-//		cout << hash[id].getWeight() << endl;
-		return hash[id];
-	}
-	cout << "ERROR - findCell found nothing" << endl;
-	return Cell();
+	nets.close();
+	return templst2;
 }
 
 float Cost1(vector<vector<Cell>> placement, vector<Net> paths)
@@ -233,18 +297,18 @@ float Cost3(vector<vector<Cell>> placement) {
 	vector<float> totalweight;
 	for (int i = 0; i < placement.size(); i++) {
 		temp = 0;
-		for (int j = 0; j < placement[0].size(); j++) {
+		for (int j = 0; j < placement[i].size(); j++) {
 			temp = temp + placement[i][j].getWeight();
 		}
 		totalweight.push_back(temp);
-		cout << temp << endl;
+		//cout << temp << endl;
 	}
 
 	//finding minimum  & maximum element
 	min = *min_element(totalweight.begin(), totalweight.end());
 	max = *max_element(totalweight.begin(), totalweight.end());
 	dif = max - min;
-	cout << dif << endl;
+	//cout << dif << endl;
 	return dif;
 
 }
@@ -258,14 +322,10 @@ float Cost4(vector<vector<Cell>> placement, vector<Net> paths)
 
 	for (int i = 0; i < placement.size(); i++)
 	{
-		cout << placement[i].size() << endl;
 		if (placement[i].size() > maxRow)
 			maxRow = placement[i].size();
 	}
-	cout << maxRow << endl;
 	int colCutoff = (maxRow / 6) + 1;
-	cout << maxRow / colCutoff << endl;
-	cout << maxRow / (colCutoff + 1) << endl;
 
 	netAmt =
 	{
@@ -290,14 +350,14 @@ float Cost4(vector<vector<Cell>> placement, vector<Net> paths)
 			{
 				netAmt[fmax(strNode.getR(), j.getR())][l]++;
 			}
-			/*cout << "starting region: " << strNode.getR() << ", " << (strNode.getC()/colCutoff) << endl;
-			cout << "ending region: " << j.getR() << ", " << (j.getC() / colCutoff) << endl;
-			for (int x = 0; x < 6; x++)
-			{
-				for (int y = 0; y < 6; y++)
-					cout << netAmt[x][y];
-				cout << endl;
-			}*/
+			//cout << "starting region: " << strNode.getR() << ", " << (strNode.getC()/colCutoff) << endl;
+			//cout << "ending region: " << j.getR() << ", " << (j.getC() / colCutoff) << endl;
+			//for (int x = 0; x < 6; x++)
+			//{
+			//	for (int y = 0; y < 6; y++)
+			//		cout << netAmt[x][y];
+			//	cout << endl;
+			//}
 		}
 	}
 
@@ -307,20 +367,21 @@ float Cost4(vector<vector<Cell>> placement, vector<Net> paths)
 		{
 			if(netAmt[x][y] > maxVal)
 				maxVal = netAmt[x][y];
-			cout << netAmt[x][y] << ", ";
+			//cout << netAmt[x][y] << ", ";
 		}
-		cout << endl;
+		//cout << endl;
 	}
 
 	return maxVal;
 }
 
-float normalizeCost(float c1, float c2, float c3, float maxC1, float maxC2, float maxC3)
+float normalizeCost(float c1, float c2, float c3, float c4, float maxC1, float maxC2, float maxC3, float maxC4)
 {
 	float normC1 = c1 / maxC1;
 	float normC2 = c2 / maxC2;
 	float normC3 = c3 / maxC3;
-	return (normC1 + normC2 + normC3);
+	float normC4 = c4 / maxC4;
+	return (normC1 + normC2 + normC3 + normC4);
 }
 
 vector<vector<Cell>> Perturb(vector<vector<Cell>> table)
@@ -328,19 +389,92 @@ vector<vector<Cell>> Perturb(vector<vector<Cell>> table)
 	int randomRow1, randomCol1, randomRow2, randomCol2;
 	int randomChoice;
 	vector<vector<Cell>> newTable;
+	Cell cell, cell2, holdCell;
+	vector<Cell>::iterator it, it2;
 
 	newTable = table;
+
 	randomRow1 = rand() % table.size();
 	randomCol1 = rand() % table[randomRow1].size();
-	Cell cell = newTable[randomRow1][randomCol1];
+	cell = newTable[randomRow1][randomCol1];
+	/*cout << "cell1 pre perturb " << newTable[randomRow1][randomCol1].getId() << " " << newTable[randomRow1][randomCol1].getR() << "," << 
+		newTable[randomRow1][randomCol1].getC() << endl;*/
+
+	randomRow2 = rand() % table.size();
+	randomCol2 = rand() % table[randomRow2].size();
+
 	randomChoice = rand() % 2;
 	if (randomChoice == 0)
 	{
+		cell2 = newTable[randomRow2][randomCol2];
+		/*cout << "cell2 pre perturb " << newTable[randomRow2][randomCol2].getId() << " " << newTable[randomRow2][randomCol2].getR() << "," <<
+			newTable[randomRow2][randomCol2].getC() << endl;*/
 
+		holdCell.setId(cell.getId());
+		holdCell.setWeight(cell.getWeight());
+		cell.setId(cell2.getId());
+		cell.setWeight(cell2.getWeight());
+		cell2.setId(holdCell.getId());
+		cell2.setWeight(holdCell.getWeight());
+
+		newTable[randomRow1][randomCol1] = cell;
+		newTable[randomRow2][randomCol2] = cell2;
+
+		/*cout << "cell1 post perturb " << newTable[randomRow1][randomCol1].getId() << " " << newTable[randomRow1][randomCol1].getR() << "," <<
+			newTable[randomRow1][randomCol1].getC() << endl;
+		cout << "cell2 post perturb " << newTable[randomRow2][randomCol2].getId() << " " << newTable[randomRow2][randomCol2].getR() << "," <<
+			newTable[randomRow2][randomCol2].getC() << endl;*/
 	}
+
 	else
 	{
+		it = newTable[randomRow1].begin();
+		it2 = newTable[randomRow2].begin();
+		newTable[randomRow1].erase(it + randomCol1);
 
+		for (int i = randomCol1; i < newTable[randomRow1].size(); i++)
+		{
+			newTable[randomRow1][i].setC(newTable[randomRow1][i].getC() - 1);
+		}
+
+		/*cout << "new cell1 " << newTable[randomRow1][randomCol1].getId() << " " << newTable[randomRow1][randomCol1].getR() << "," <<
+			newTable[randomRow1][randomCol1].getC() << endl;*/
+
+		cell.setR(randomRow2);
+		cell.setC(randomCol2);
+
+		newTable[randomRow2].insert(it2 + randomCol2, cell);
+
+
+		/*cout << "cell1 post perturb " << newTable[randomRow2][randomCol2].getId() << " " << newTable[randomRow2][randomCol2].getR() << "," <<
+			newTable[randomRow2][randomCol2].getC() << endl;*/
+
+		for (int i = randomCol2 + 1; i < newTable[randomRow2].size(); i++)
+		{
+			newTable[randomRow2][i].setC(newTable[randomRow2][i].getC() + 1);
+		}
 	}
+
 	return newTable;
+}
+
+float increaseProbability(float deltaC, float temp)
+{
+	float exponent = (deltaC * -1) / temp;
+	float result = exp(exponent);
+	return result;
+}
+
+float tempSchedule(float curTemperature)
+{
+	float newTemp;
+
+	if (curTemperature > 15000)
+		newTemp = curTemperature * .8;
+	else if(curTemperature > 5000)
+		newTemp = curTemperature * .95;
+	else
+		newTemp = curTemperature * .8;
+
+	return newTemp;
 }
